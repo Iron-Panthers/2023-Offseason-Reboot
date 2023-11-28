e625e5ff1a6ae55bc9a005e20350163a288e267c
Heggiehog <nizcat5@gmail.com>
2023-11-27 17:17:40 -0800

    Capitalized Commands ... hehe

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.WristCommand;
+import frc.robot.Commands.WristCommand;
 import frc.robot.subsystems.WristSubsystem;
 
 /**

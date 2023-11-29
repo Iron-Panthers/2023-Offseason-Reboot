146906696a684d9cd0774075f8d8b5f2837e3d71
Bruce <brucebenjaminpeters@gmail.com>
2023-11-28 17:38:21 -0800

    Bug FIx

--- src/main/java/frc/robot/Commands/IntakeCommand.java
+++ src/main/java/frc/robot/Commands/IntakeCommand.java
@@ -52,6 +52,6 @@ public class IntakeCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return intakeSubsystem.IsLoaded() || !isIntake;
+    return intakeSubsystem.IsLoaded();
   }
 }

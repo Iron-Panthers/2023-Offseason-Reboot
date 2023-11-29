b5d28a1fbb17171da7fc3d1fb452d03949655234
Noland <nolan.bjorn.schuler@gmail.com>
2023-11-28 18:41:53 -0800

    Good Day :) productive
    Created the PID (everything set to 0), added a target angle, added and fixed the gear ratio, added Values to the shuffleboard, and set the motor power in periodic, and added the TalonFX motor

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -4,8 +4,10 @@
 
 
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,35 +15,47 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class WristSubsystem extends SubsystemBase {
  /** Creates a new ExampleSubsystem. */
- private TalonFX left_motor;
+ private TalonFX motor;
  /** leader */
- private TalonFX right_motor;
  private PIDController pidController;
- private final ShuffleboardTab ElevatorTab = Shuffleboard.getTab("Elevator");
+ private final ShuffleboardTab WristTab = Shuffleboard.getTab("Elevator");
 
 
- private double targetHeight;
+ private double targetAngle;
  private double motorPower;
-  public WristSubsystem() {
-
-
-
-
-
-
-
-
-
- }
  
+ //creates the WristSubsystem 
+  public WristSubsystem() {
+    motor = new TalonFX(16);
+    motorPower = 0;
+    targetAngle = 0; //sets the "targetAngle" variable to 0 by default
+
+    //creates the new PID :) WOOHOOOO
+    pidController = new PIDController(0, 0, 0);
+
+
+    //
+    WristTab.addNumber("Current Motor Power", () -> motorPower);  
+    WristTab.addNumber("Target Angle", () -> this.targetAngle);
+  }
+ 
+
+  public static double inchesToTicks(double height) {
+    return height * ((Wrist.GEAR_RATIO * Wrist.TICKS_PER_REVOLUTION) / (Wrist.GEAR_CIRCUMFERENCE));
+  }
+  public static double ticksToInches(double ticks) {
+    return (ticks * Wrist.GEAR_CIRCUMFERENCE) / (Wrist.TICKS_PER_REVOLUTION * Wrist.GEAR_RATIO);
+  }
+
+
+// updates the "targetAngle" variable if you change the value in WristCommand in public void execute
+public void setTargetAngle(double targetAngle) {
+  this.targetAngle = targetAngle;
+}
+
  @Override
  public void periodic() {
+   motor.set(TalonFXControlMode.PercentOutput, motorPower);
    
  }
-
-
- @Override
- public void simulationPeriodic() {
-   // This method will be called once per scheduler run during simulation
-   }
- }
+}

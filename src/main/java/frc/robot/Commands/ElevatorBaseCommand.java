5ca5b766f7b49c101c1b21ac93fbb47ff4e3b867
audrey <audreypjohnson@gmail.com>
2023-11-30 18:04:03 -0800

    fixing subsystem pathing

--- src/main/java/frc/robot/Commands/ElevatorBaseCommand.java
+++ src/main/java/frc/robot/Commands/ElevatorBaseCommand.java
@@ -2,46 +2,47 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.commands;
 
-import frc.Subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 /** An example command that uses an example subsystem. */
 public class ElevatorBaseCommand extends CommandBase {
- @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
- private final ElevatorSubsystem elevatorSubsystem;
-public double targetHeight;
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ElevatorSubsystem elevatorSubsystem;
 
- /**
-  * Creates a new ExampleCommand.
-  *
-  * @param subsystem The subsystem used by this command.
-  */
- public ElevatorBaseCommand(ElevatorSubsystem elevatorSubsystem, double targetHeight) {
-   this.targetHeight = targetHeight;
-   this.elevatorSubsystem = elevatorSubsystem;
-   // Use addRequirements() here to declare subsystem dependencies.
-   addRequirements(elevatorSubsystem);
- }
+  public double targetHeight;
 
- // Called when the command is initially scheduled.
- @Override
- public void initialize() {
-   elevatorSubsystem.setTargetHeight(targetHeight);
- }
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ElevatorBaseCommand(ElevatorSubsystem elevatorSubsystem, double targetHeight) {
+    this.targetHeight = targetHeight;
+    this.elevatorSubsystem = elevatorSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(elevatorSubsystem);
+  }
 
- // Called every time the scheduler runs while the command is scheduled.
- @Override
- public void execute() {}
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    elevatorSubsystem.setTargetHeight(targetHeight);
+  }
 
- // Called once the command ends or is interrupted.
- @Override
- public void end(boolean interrupted) {}
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
 
- // Returns true when the command should end.
- @Override
- public boolean isFinished() {
-   return elevatorSubsystem.nearTargetHeight();
- }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return elevatorSubsystem.nearTargetHeight();
+  }
 }

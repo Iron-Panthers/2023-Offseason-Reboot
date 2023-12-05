8aeac1c98b5f1e53b5166a8ab4f90b900fc6fbca
driverstation <5026craptop@gmail.com>
2023-12-04 17:22:28 -0800

    name changes

--- src/main/java/frc/robot/Commands/WristCommand.java
+++ src/main/java/frc/robot/Commands/WristCommand.java
@@ -5,31 +5,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.WristSubsystem;
 
-/** An example command that uses an example subsystem. */
-public class ElevatorBaseCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ElevatorSubsystem elevatorSubsystem;
+public class WristCommand extends CommandBase {
+  /** Creates a new WristCommand. */
+  private final WristSubsystem wristSubsystem;
 
-  public double targetHeight;
+  private double targetAngle;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ElevatorBaseCommand(ElevatorSubsystem elevatorSubsystem, double targetHeight) {
-    this.targetHeight = targetHeight;
-    this.elevatorSubsystem = elevatorSubsystem;
+  public WristCommand(WristSubsystem wristSubsystem, double targetAngle) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(elevatorSubsystem);
+    this.wristSubsystem = wristSubsystem;
+    this.targetAngle = targetAngle;
+
+    addRequirements(wristSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    elevatorSubsystem.setTargetHeight(targetHeight);
+    // sets target angle
+    wristSubsystem.setTargetAngle(targetAngle);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -43,6 +39,6 @@ public class ElevatorBaseCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return elevatorSubsystem.nearTargetHeight();
+    return wristSubsystem.nearTargetAngle();
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,62 +2,52 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.Commands.ElevatorBaseCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.Subsystems.ElevatorSubsystem;
-
+import frc.robot.commands.ElevatorBaseCommand;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 /**
-* This class is where the bulk of the robot should be declared. Since Command-based is a
-* "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
-* periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-* subsystems, commands, and button mappings) should be declared here.
-*/
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
- // The robot's subsystems and commands are defined here...
- private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+  // The robot's subsystems and commands are defined here...
+  private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
 
+  private final ElevatorBaseCommand ElevatorBaseCommand =
+      new ElevatorBaseCommand(elevatorSubsystem, 0);
 
-private final ElevatorBaseCommand ElevatorBaseCommand = new ElevatorBaseCommand(elevatorSubsystem, 0);
+  private final CommandXboxController driverA = new CommandXboxController(0);
+  private double targetHeight;
 
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
 
- private final CommandXboxController driverA = new CommandXboxController(0);
- private double targetHeight;
+    // Configure the button bindings
+    configureButtonBindings();
+    driverA.y().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 20));
+    driverA.x().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 15d));
+    driverA.b().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 10d));
+    driverA.a().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 3d));
+  }
 
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
 
- /** The container for the robot. Contains subsystems, OI devices, and commands. */
- public RobotContainer() {
-
-
-  
-
-   // Configure the button bindings
-   configureButtonBindings();
-   driverA.y().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 20));
-   driverA.x().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 15d));
-   driverA.b().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 10d));
-   driverA.a().onTrue(new ElevatorBaseCommand(elevatorSubsystem, 3d));
- }
-
-
- /**
-  * Use this method to define your button->command mappings. Buttons can be created by
-  * instantiating a {@link GenericHID} or one of its subclasses ({@link
-  * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-  * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-  */
- private void configureButtonBindings() {}
-
-
- /**
-  * Use this to pass the autonomous command to the main {@link Robot} class.
-  *
-  * @return the command to run in autonomous
-  */
- public Command getAutonomousCommand() {
-   // An ExampleCommand will run in autonomous
-   return new InstantCommand();
- }
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new InstantCommand();
+  }
 }
-
-
